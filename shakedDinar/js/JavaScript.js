b25f2aeb98f2385dc92688bd05c095a94fbaee66
Shaked-Dinar <shaked.dinar1@gmail.com>
2020-01-19 18:33:11 +0200

    Add files via upload

---
+++ shakedDinar/js/JavaScript.js
@@ -0,0 +1,28 @@
+﻿function readMore(parameter) {
+    var num = parameter[0];
+    if (parameter.includes('open')) {
+        $("#readMoreBtn").attr("onclick", "readMore('"+num+"close')");
+        $("#project"+num+" .readMore").css("display", "block");
+        $("#readMoreBtn").html("Read Less");
+      }
+    else {
+        $("#readMoreBtn").attr("onclick", "readMore('" + num + "open')");
+        $("#project" + num + " .readMore").css("display", "none");
+        $("#readMoreBtn").html("Read More")
+    }
+}
+
+function showHide(parameter) {
+    if (parameter == 'show') {
+        $("#show").attr("onclick", "showHide('hide')");
+        $(".wireframes").css("display", "block");
+        $("#show").html("Hide Wireframes")
+      //  $(".wireframes")[0].scrollIntoView({ block: 'start', behavior: 'smooth' });
+
+    } else {
+        $("#show").attr("onclick", "showHide('show')");
+        $(".wireframes").css("display", "none");
+        $("#show").html("Show Wireframes")
+        $("#show").scrollIntoView({ block: 'start', behavior: 'smooth' });
+    }
+}
